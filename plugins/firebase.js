fb2b2c8df764d50f3ca71a023b1364c972501753
keni1211 <k.auction1211@gmail.com>
2020-11-19 05:41:58 +0900

    firebaseの設定

---
+++ plugins/firebase.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase'
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(
+    {
+        apiKey: "AIzaSyDgzhhad_ww5YjFQhxIx5QIeL2UVNjky7Q",
+        authDomain: "fishing-club-62cb2.firebaseapp.com",
+        databaseURL: "https://fishing-club-62cb2.firebaseio.com",
+        projectId: "fishing-club-62cb2",
+        storageBucket: "fishing-club-62cb2.appspot.com",
+        messagingSenderId: "116139461972",
+        appId: "1:116139461972:web:0b4fa7432f533904350cc1",
+        measurementId: "G-TX85VEE11W"
+    }
+  )
+}
+
+export default firebase
